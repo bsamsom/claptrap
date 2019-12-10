b0e066459163d03aabc94c8ddabb6996b856311e
bsamsom <bsamsom@mts.net>
2019-12-10 14:50:17 -0600

    Added server and role listing functions

--- Discord_bot.js
+++ Discord_bot.js
@@ -1,80 +1,107 @@
 const Discord = require('discord.js')
+const config = require("./config.js")
 const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
-
     //set the bot to `playing With JavaScript` 
     //client.user.setActivity("With JavaScript")
     //Set the bot to watching youtube
     client.user.setActivity("Youtube", {type: "WATCHING"})
-
-    // List servers the bot is connected to
-    console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-         // List all channels
-         guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-        })
-    })
 })
 
 
-client.on('message', (receivedMessage) => {
+client.on('message', (message) => {
     // Prevent bot from responding to its own messages
-    if (receivedMessage.author == client.user) {
+    if (message.author == client.user) {
         return
     }
-
-
-    if(receivedMessage.content.includes("food") == true){
-        //const webAttachment = new Discord.Attachment('https://gph.is/YBDKK8')
-        //const localFileAttachment = new Discord.Attachment('C:\\Users\\Benjamin Samsom\\Downloads\\pics\\57b7fada069d3a1ebd3a3c5dd7bb20ad.png')
-        //receivedMessage.channel.send(localFileAttachment)
-        receivedMessage.react("🥞")
+    if (message.content.startsWith("!")) {
+        processCommand(message)
     }
-
-
-    //receivedMessage.channel.send("Message received: " + receivedMessage.content)
-    // Tag the user
-    //receivedMessage.channel.send("Message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
-    if (receivedMessage.content.startsWith("!")) {
-        processCommand(receivedMessage)
+    else{
+        messageContains(message)
     }
 })
 
-function processCommand(receivedMessage) {
-    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
-    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+function messageContains(message) {
+    message = message.content
+    if(message.includes("food")){
+        message.react("🥞")
+    }
+}
 
-    console.log("Command received: " + primaryCommand)
+function processCommand(message) {
+    fullCommand = message.content.substr(1) // Remove the leading exclamation mark
+    splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+    user = getUser(message)
+    guild = message.guild;//get the current guild
+
+    console.log("Command received: " + primaryCommand + " sent by user: " + user.toString() + " " + user.displayName)
     console.log("Arguments: " + arguments) // There may not be any arguments
 
     if (primaryCommand == "help") {
-        helpCommand(arguments, receivedMessage)
+        helpCommand(arguments, message)
+    }else if (primaryCommand == "listServers") {
+        if(checkRole(user,["Admin"])){
+            listServers()
+        }else{
+            message.channel.send("You dont have access to the `!list` command")
+        }
+    }else if (primaryCommand == "listMembers") {
+        if(checkRole(user,["Admin"])){
+            listMembersWithRole(user, message, arguments)
+        }else{
+            message.channel.send("You dont have access to the `!list` command")
+        } 
     }else {
-        receivedMessage.channel.send("I don't understand the command. Try `!help`")
+        message.channel.send("I don't understand the command. Try `!help`")
     }
 }
 
-function helpCommand(arguments, receivedMessage) {
+function checkRole(user, requiredRole){
+    return user.roles.some(role=>requiredRole.includes(role.name))
+}
+
+function listMembersWithRole(user, message, roles){
+    guild = message.guild;
+    roles.forEach((role) => {
+        myRole = message.guild.roles.find(r => r.name === role)
+        membersWithRole = message.guild.roles.get(myRole.id).members
+        printString = `there are ${membersWithRole.size} members with the Role of: ${role}\n`
+        membersWithRole.forEach((user) => {
+            member = guild.member(user)
+            printString += "\t" + member.displayName + "\n"
+        })
+        message.channel.send(printString)
+    })
+}
+
+function getUser(message){
+    guild = message.guild;
+    return guild.member(message.author)
+}
+
+function helpCommand(arguments, message) {
     if (arguments.length > 0) {
-        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+        message.channel.send("It looks like you might need help with " + arguments)
     } else {
-        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+        message.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
     }
 }
 
+function listServers(){
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name + " " + guild.id)
+         // List all channels of each server
+         guild.channels.forEach((channel) => {
+            console.log(`\t${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
+}
 
-
-
-
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = ""
-
-client.login(bot_secret_token)
+client.login(config.DISCORD_BOT_TOKEN)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Discord_Bot
+
+Steps to run:
+1. Npm install
+2. Npm install discord.js
+3. Find your botsa token, by going to https://discordapp.com/developers/applications/ and finding your server.
+4. Click on your application -> Bot -> Token -> "Copy"
+5. Set your discord bots token in a config.js like this: 
+    module.exports = {
+        DISCORD_BOT_TOKEN : ""   
+    }
+6. run node Discord_bot.js to start the bot.
+7. hit CTRL C to stop the bot.
